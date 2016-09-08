a7cf4cacc8bf537c7a2f21666991aedd916e7d9f
IanStorm <IanStorm28@outlook.com>
2016-09-08 15:26:16 +0200

    * implemented basic navigation
    * improved styling of HamburgerMenu

--- UI/UWP/RYB/Modules/Main/MainPage.xaml.cs
+++ UI/UWP/RYB/Modules/Main/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using XElement.RedYellowBlue.UI.UWP.Modules;
+using XElement.RedYellowBlue.UI.UWP.Modules.Main;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -10,12 +13,34 @@ namespace XElement.RedYellowBlue.UI.UWP
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.NavigateTo( NavigationOptions.Home );
         }
 
 
-        private void HamburgerMenu_OptionsItemClick( object sender, ItemClickEventArgs e )
+        private void HamburgerMenu_ItemClick( object sender, ItemClickEventArgs e )
         {
-            // TODO
+            if ( e.ClickedItem == this._homeItem )
+                this.NavigateTo( NavigationOptions.Home );
+            else if ( e.ClickedItem == this._settingsItem )
+                this.NavigateTo( NavigationOptions.Settings );
+            else /*if ( e.ClickedItem == this._aboutItem )*/
+                this.NavigateTo( NavigationOptions.About );
+        }
+
+
+        private void NavigateTo( NavigationOptions navOptions )
+        {
+            Type navigateTo = null;
+
+            if ( navOptions == NavigationOptions.Home )
+                navigateTo = typeof( HomePage );
+            else if ( navOptions == NavigationOptions.Settings )
+                navigateTo = typeof( SettingsPage );
+            else /*if ( navOptions == NavigationOptions.About )*/
+                navigateTo = null; // TODO
+
+            this._navigationFrame.Navigate( navigateTo );
         }
     }
 #endregion
