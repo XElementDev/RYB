7be5a9e7b2cdcdc13a623b0bc9e6aa99550ba09b
IanStorm <IanStorm28@outlook.com>
2016-09-23 13:15:47 +0200

    * refactoring: created TabHeaderUC

---
+++ UI/UWP/Modules/TabHeader/TabHeaderUC/View.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace XElement.RedYellowBlue.UI.UWP.Modules
+{
+#region not unit-tested
+    public sealed partial class TabHeaderUC : UserControl
+    {
+        public TabHeaderUC()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+
+        public string Glyph
+        {
+            get { return (string)this.GetValue( TabHeaderUC.GlyphProperty ); }
+            set { this.SetValue( TabHeaderUC.GlyphProperty, value ); }
+        }
+
+        public static readonly DependencyProperty GlyphProperty = 
+            DependencyProperty.Register( nameof( Glyph ), typeof( string ), 
+                                         typeof( TabHeaderUC ), 
+                                         new PropertyMetadata( String.Empty ) );
+
+
+        public string Label
+        {
+            get { return (string)this.GetValue( TabHeaderUC.LabelProperty ); }
+            set { this.SetValue( TabHeaderUC.LabelProperty, value ); }
+        }
+
+        public static readonly DependencyProperty LabelProperty = 
+            DependencyProperty.Register( nameof( Label ), typeof( string ), typeof( TabHeaderUC ), 
+                                         new PropertyMetadata( String.Empty ) );
+    }
+#endregion
+}
