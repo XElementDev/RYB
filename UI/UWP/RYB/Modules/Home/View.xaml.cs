a937733750d02eaebfc6f61532cbbf9f71a8a74d
IanStorm <IanStorm28@outlook.com>
2016-09-23 10:16:46 +0200

    * introduced pivot in home module to distinguish between devices, groups and favorites

--- UI/UWP/RYB/Modules/Home/View.xaml.cs
+++ UI/UWP/RYB/Modules/Home/View.xaml.cs
@@ -5,9 +5,6 @@
 namespace XElement.RedYellowBlue.UI.UWP.Modules
 {
 #region not unit-tested
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class HomePage : Page
     {
         public HomePage()
