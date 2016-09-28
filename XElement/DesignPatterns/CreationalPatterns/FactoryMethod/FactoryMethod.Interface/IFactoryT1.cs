44c1b76411afa6a4f1d6914c28ab4efec3458921
IanStorm <IanStorm28@outlook.com>
2016-09-28 21:58:55 +0200

    * added FactoryMethod design pattern project

---
+++ XElement/DesignPatterns/CreationalPatterns/FactoryMethod/FactoryMethod.Interface/IFactoryT1.cs
@@ -0,0 +1,7 @@
+﻿namespace XElement.DesignPatterns.CreationalPatterns.FactoryMethod
+{
+    public interface IFactory<TOut>
+    {
+        TOut Get();
+    }
+}
---
+++ XElement/DesignPatterns/CreationalPatterns/FactoryMethod/FactoryMethod.Interface/IFactoryT2.cs
@@ -0,0 +1,7 @@
+﻿namespace XElement.DesignPatterns.CreationalPatterns.FactoryMethod
+{
+    public interface IFactory<TOut, TIn>
+    {
+        TOut Get( TIn parameter );
+    }
+}
