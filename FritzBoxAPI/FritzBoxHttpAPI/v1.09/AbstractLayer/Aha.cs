fab0145faf4650df645805d6f369be877a9f7108
IanStorm <IanStorm28@outlook.com>
2016-10-04 20:53:23 +0200

    * added SetSwitchOff and SetSwitchOn to HTTP API

--- FritzBoxAPI/FritzBoxHttpAPI/v1.09/AbstractLayer/Aha.cs
+++ FritzBoxAPI/FritzBoxHttpAPI/v1.09/AbstractLayer/Aha.cs
@@ -13,11 +13,18 @@ namespace XElement.RedYellowBlue.FritzBoxAPI.FritzBoxHttpAPI.v109
         }
 
 
+        private AhaRequest CreateAhaRequest( AhaCmd cmd )
+        {
+            var ahaRequest = new AhaRequest( this._boxUrl, cmd, this._sid );
+            return ahaRequest;
+        }
+
+
         public DeviceListDTO GetDeviceListInfos()
         {
             DeviceListDTO result = null;
 
-            var ahaRequest = new AhaRequest( this._boxUrl, AhaCmd.getdevicelistinfos, this._sid );
+            var ahaRequest = this.CreateAhaRequest( AhaCmd.getdevicelistinfos );
             var response = ahaRequest.Create().GetResponseAsync().Result;
             using ( var stream = response.GetResponseStream() )
             {
@@ -26,6 +33,38 @@ namespace XElement.RedYellowBlue.FritzBoxAPI.FritzBoxHttpAPI.v109
             }
 
             return result;
+        }
+
+
+        //private void ParseResponse( WebResponse response )
+        //{
+        //    var httpResponse = response as HttpWebResponse;
+        //    if ( httpResponse.StatusCode != HttpStatusCode.OK )
+        //    {
+        //        throw new Exception( "Request not successful." );
+        //    }
+        //}
+
+
+        private void SetSwitch( string ain, bool targetState )
+        {
+            var cmd = targetState == false ? AhaCmd.setswitchoff : AhaCmd.setswitchon;
+            var ahaRequest = this.CreateAhaRequest( cmd );
+            ahaRequest.Ain = ain;
+            var response = ahaRequest.Create().GetResponseAsync().Result;
+            //this.ParseResponse( response );
+        }
+
+
+        public void SetSwitchOff( string ain )
+        {
+            this.SetSwitch( ain, false );
+        }
+
+
+        public void SetSwitchOn( string ain )
+        {
+            this.SetSwitch( ain, true );
         }
 
 
