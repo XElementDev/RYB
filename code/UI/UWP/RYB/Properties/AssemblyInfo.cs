15682275f946969cc501c27081174ece0eb4f703
IanStorm <IanStorm28@outlook.com>
2016-10-27 14:44:13 +0200

    * "synced" package info and assembly info files

--- code/UI/UWP/RYB/Properties/AssemblyInfo.cs
+++ code/UI/UWP/RYB/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "RYB" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyTitle( "RYB - RedYellowBlue" )]
+[assembly: AssemblyDescription( "An UWP app for controlling the smart home functionality of your Fritz!Box." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "RYB" )]
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "0.0.0.0" )]
-[assembly: AssemblyFileVersion( "0.0.0.0" )]
+[assembly: AssemblyVersion( "0.1.0.0" )]
+[assembly: AssemblyFileVersion( "0.1.0.0" )]
 [assembly: ComVisible( false )]
