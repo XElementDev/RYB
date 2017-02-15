14c13b6a6cad01049cb42c3d1f2d435b3175a2de
IanStorm <IanStorm28@outlook.com>
2017-02-15 15:58:58 +0100

    * added some TODOs

--- code/FritzBoxAPI/FritzBoxHttpAPI/AHA/v1.09+/NextChangeDTO.cs
+++ code/FritzBoxAPI/FritzBoxHttpAPI/AHA/v1.09+/NextChangeDTO.cs
@@ -5,12 +5,14 @@ namespace XElement.RedYellowBlue.FritzBoxAPI.HttpApi.Aha.v109_
 #region not unit-tested
     public class NextChangeDTO
     {
-        //  --> TODO
+        //  --> TODO: Currently undocumented by AVM.
+        // -->  TODO: Change return type.
         [XmlElement( "endperiod" )]
         public string EndPeriod { get; set; }
 
 
-        //  --> TODO
+        //  --> TODO: Currently undocumented by AVM.
+        // -->  TODO: Change return type.
         [XmlElement( "tchange" )]
         public string TChange { get; set; }
     }
