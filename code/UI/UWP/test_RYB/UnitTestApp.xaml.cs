b145482b88b8a21471f5efd8c8fc3a35184a2085
IanStorm <IanStorm28@outlook.com>
2017-02-26 18:36:10 +0100

    * adapted namespace of unit test project

--- code/UI/UWP/test_RYB/UnitTestApp.xaml.cs
+++ code/UI/UWP/test_RYB/UnitTestApp.xaml.cs
@@ -1,32 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace test_RYB
+namespace XElement.RedYellowBlue.UI.UWP
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class test_App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
-        public App()
+        public test_App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
