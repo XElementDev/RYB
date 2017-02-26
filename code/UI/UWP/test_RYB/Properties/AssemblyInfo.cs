428f2de13f4b2d417c90d8cc6f350295c1f05735
IanStorm <IanStorm28@outlook.com>
2017-02-26 18:37:20 +0100

    * adapted AssemblyInfo of unit test project

--- code/UI/UWP/test_RYB/Properties/AssemblyInfo.cs
+++ code/UI/UWP/test_RYB/Properties/AssemblyInfo.cs
@@ -1,19 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("test_RYB")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test_RYB")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyMetadata("TargetPlatform","UAP")]
+[assembly: AssemblyTitle( "test_RYB" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "RYB" )]
+[assembly: AssemblyCopyright( "Copyright © XElement 2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyMetadata( "TargetPlatform", "UAP" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -25,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyVersion( "0.0.0.0" )]
+[assembly: AssemblyFileVersion( "0.0.0.0" )]
+[assembly: ComVisible( false )]
