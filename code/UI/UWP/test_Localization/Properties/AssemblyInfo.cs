0895470cb0868903cb89ff013f090ac5834f4080
IanStorm <IanStorm28@outlook.com>
2017-09-29 14:41:06 +0200

    * added empty unit test project for localization

---
+++ code/UI/UWP/test_Localization/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "Test_XElement.RYB.UI.UWP.Localization" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "RYB" )]
+[assembly: AssemblyCopyright( "Copyright © XElement 2017" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyMetadata( "TargetPlatform", "UAP" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "0.0.0.0" )]
+[assembly: AssemblyFileVersion( "0.0.0.0" )]
+[assembly: ComVisible( false )]
