72f75f7e6c0d26c0db1699e8089734c55795db02
IanStorm <IanStorm28@outlook.com>
2017-10-02 14:25:19 +0200

    * updated timestamp of the copyright

--- code/UI/UWP/Modules/GitHubLink/Properties/AssemblyInfo.cs
+++ code/UI/UWP/Modules/GitHubLink/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "RYB" )]
-[assembly: AssemblyCopyright( "Copyright © XElement 2016" )]
+[assembly: AssemblyCopyright( "Copyright © XElement 2016-2017" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
