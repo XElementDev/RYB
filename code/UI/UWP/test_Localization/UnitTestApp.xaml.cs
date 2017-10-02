ae4405fbe88d0f63e75ef02b68323b1f1df05b86
IanStorm <IanStorm28@outlook.com>
2017-10-02 11:45:38 +0200

    * removed some TODOs from unit test project

--- code/UI/UWP/test_Localization/UnitTestApp.xaml.cs
+++ code/UI/UWP/test_Localization/UnitTestApp.xaml.cs
@@ -50,7 +50,6 @@ namespace XElement.RYB.UI.UWP.Localization
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    //TODO: Load state from previously suspended application
                 }
 
                 // Place the frame in the current Window
@@ -85,7 +84,6 @@ namespace XElement.RYB.UI.UWP.Localization
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
